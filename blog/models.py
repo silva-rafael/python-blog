c90960c2f8aecae84187534cc6a49046d988871a
rafael <rafaelribeirodasilva7@gmail.com>
2024-12-14 18:08:05 -0300

    Criado o primeiro post, com as configurações necessárias.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100, default='Novo Post', verbose_name='Título')
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/%Y/%m')
+    created_ad = models.DateTimeField(auto_now_add=True)
+    updated_ad = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
 def home_view(request):
-    return render(request, 'blog/home.html')
+    posts = Post.objects.all()
+    return render(request, 'blog/home.html', {'posts' : posts})
